087507385eebdcbf9fc275c941fffdf0ffb17bf6
EmileTrotignon <emile.trotignon@gmail.com>
2026-05-20 20:01:49 +0200

    more complex screenshot test

--- test/cram/js.t/basic/src/basic.ml
+++ test/cram/js.t/basic/src/basic.ml
@@ -1,6 +1,89 @@
 open Gamelle
 
+let w = 800.
+let h = 600.
+let cell_w = w /. 3.
+let cell_h = h /. 2.
+
+let cell col row =
+  Box.v
+    (Point.v (float col *. cell_w) (float row *. cell_h))
+    (Size.v cell_w cell_h)
+
+let label ~io b txt =
+  Text.draw ~io ~color:Color.white ~size:14
+    ~at:(Point.v (Box.x_left b +. 4.) (Box.y_top b +. 4.))
+    txt
+
 let () =
-  Gamelle.run () @@ fun ~io () ->
-  Box.fill ~io ~color:Color.black (Box.v (Point.zero) (Size.v 300. 300.));
-  Text.draw ~io "Hello Gamelle!" ~color:Color.red ~at:(Point.v 10. 10.)
+  run () @@ fun ~io () ->
+  if Input.is_pressed ~io `escape then raise Exit;
+  let io = View.drawing_box (Box.v Point.zero (Size.v w h)) io in
+  Box.fill ~io ~color:Color.(rgb 40 40 40) (Window.box ~io);
+
+  (* (0,0) Segment *)
+  let b = cell 0 0 in
+  label ~io b "Segment.draw";
+  Segment.draw ~io ~color:Color.cyan
+    (Segment.v
+       (Point.v (Box.x_left b +. 10.) (Box.y_top b +. 30.))
+       (Point.v (Box.x_right b -. 10.) (Box.y_bottom b -. 10.)));
+  Segment.draw ~io ~color:Color.orange
+    (Segment.v
+       (Point.v (Box.x_right b -. 10.) (Box.y_top b +. 30.))
+       (Point.v (Box.x_left b +. 10.) (Box.y_bottom b -. 10.)));
+
+  (* (1,0) Box draw + fill *)
+  let b = cell 1 0 in
+  label ~io b "Box.draw / fill";
+  let inner =
+    Box.v
+      (Point.v (Box.x_left b +. 10.) (Box.y_top b +. 25.))
+      (Size.v (cell_w -. 20.) (cell_h -. 35.))
+  in
+  Box.fill ~io ~color:Color.blue inner;
+  Box.draw ~io ~color:Color.yellow inner;
+
+  (* (2,0) Circle draw + fill *)
+  let b = cell 2 0 in
+  label ~io b "Circle.draw / fill";
+  let r = Float.min (cell_w /. 4.) (cell_h /. 4.) -. 5. in
+  let mx, my = (Box.x_middle b, Box.y_middle b +. 10.) in
+  Circle.fill ~io ~color:Color.red (Circle.v (Point.v (mx -. r -. 5.) my) r);
+  Circle.draw ~io ~color:Color.lime (Circle.v (Point.v (mx +. r +. 5.) my) r);
+
+  (* (0,1) Polygon draw + fill *)
+  let b = cell 0 1 in
+  label ~io b "Polygon.draw / fill";
+  let c = Box.center b in
+  let cx, cy = (Point.x c, Point.y c) in
+  let tri =
+    Polygon.v
+      [
+        Point.v cx (cy -. 65.);
+        Point.v (cx +. 60.) (cy +. 40.);
+        Point.v (cx -. 60.) (cy +. 40.);
+      ]
+  in
+  Polygon.fill ~io ~color:Color.magenta tri;
+  Polygon.draw ~io ~color:Color.gold tri;
+
+  (* (1,1) Touching boxes — 4 cells sharing exact edges *)
+  let b = cell 1 1 in
+  label ~io b "Touching boxes";
+  let bx = Box.x_left b +. 10. and by = Box.y_top b +. 25. in
+  let bw = (cell_w -. 20.) /. 2. and bh = (cell_h -. 35.) /. 2. in
+  Box.fill ~io ~color:Color.teal   (Box.v (Point.v bx           by        ) (Size.v bw bh));
+  Box.fill ~io ~color:Color.coral  (Box.v (Point.v (bx +. bw)   by        ) (Size.v bw bh));
+  Box.fill ~io ~color:Color.indigo (Box.v (Point.v bx           (by +. bh)) (Size.v bw bh));
+  Box.fill ~io ~color:Color.violet (Box.v (Point.v (bx +. bw)   (by +. bh)) (Size.v bw bh));
+
+  (* (2,1) Bitmap + text *)
+  let b = cell 2 1 in
+  label ~io b "Bitmap + text";
+  draw ~io Assets.camel ~at:(Point.v (Box.x_left b +. 10.) (Box.y_top b +. 25.));
+  Text.draw ~io ~color:Color.crimson ~size:20
+    ~at:(Point.v (Box.x_left b +. 10.) (Box.y_bottom b -. 35.))
+    "Hello Gamelle!";
+
+  ()
--- test/cram/js.t/run.t
+++ test/cram/js.t/run.t
@@ -15,26 +15,60 @@
   basic_js.ml
   basic_js.mli
   src
-
   $ ../screenshot/screenshot.exe _build/default/basic/basic.html > image.png
   $ echo $PWD
   $TESTCASE_ROOT
+Compare against the reference screenshot (promote reference.png when intentionally changing the rendering)
+
   $ odiff reference.png image.png diff.png || true
-  Images are identical
+  Found 399807 different pixels (82.81%)
+
+Check the canvas size matches the View.drawing_box dimensions (800x600)
+
   $ magick identify -format "size: %wx%h\n" image.png
-  size: 1282x962
-  $ magick image.png +dither -colors 8 txt:- | awk 'NR>1{print $3}' | sort | uniq -c | sort -rn
-  1232712 #000000FF
-      190 #640000FF
-      168 #DA0000FF
-      110 #A20000FF
-      104 #2F0000FF
+  size: 802x602
+
+Count pixels per exact color, filtering out colors with fewer than 500 pixels
+(anti-aliasing noise). Each significant color should correspond to a specific
+draw call:
+background:          Color.(rgb 40 40 40)  = #282828FF
+labels (Text.draw):  Color.white           = #FFFFFFFF
+Segment.draw:        Color.cyan            = #00FFFFFF
+Segment.draw:        Color.orange          = #FF8000FF
+Box.fill:            Color.blue            = #0000FFFF
+Box.draw:            Color.yellow          = #FFFF00FF
+Circle.fill:         Color.red             = #FF0000FF
+Circle.draw:         Color.lime            = #32CD32FF
+Polygon.fill:        Color.magenta         = #FF00FFFF
+Polygon.draw:        Color.gold            = #FFD700FF
+Box.fill (touch TL): Color.teal            = #008080FF
+Box.fill (touch TR): Color.coral           = #FF7F50FF
+Box.fill (touch BL): Color.indigo          = #4B0082FF
+Box.fill (touch BR): Color.violet          = #EE82EEFF
+Text.draw:           Color.crimson         = #DC143CFF
+Bitmap.draw:         Assets.camel (camel.png, mixed colors)
+
+  $ magick image.png txt:- | awk 'NR>1{print $3}' | sort | uniq -c | sort -rn | awk '$1 >= 500'
+   322658 #282828FF
+    64172 #0000FFFF
+    16236 #FF7F50FF
+    16236 #EE82EEFF
+    16236 #4B0082FF
+    16236 #008080FF
+    11723 #FF0000FF
+     5974 #FF00FFFF
+     2804 #000000FF
+      565 #C5A97FFF
+      549 #CAAF83FF
+      526 #2A2AD5FF
+
   $ sha512sum image.png
-  13bedbc5cdbdf70821140068c78717d31126ff450ff90cc5feeb94748c257ebee894b99ba1c27aebf311fda8cd267b0f9071c8ea71997f3a8389149ffb0417b3  image.png
-  $ cp image.png /tmp
-  $ cp diff.png /tmp
-  cp: cannot stat 'diff.png': No such file or directory
-  [1]
+  bf33b3d2cdaef52d7d487a29bdad603d8960185463d0c058323ec3dc8fef2c91344095d6e75b2fbfcabb6949344243904ff206f2fcb99be506d040e1f3fade61  image.png
+
+When there is a diff, uncomment the bellow to understand whats happening
+
+$ cp image.png /tmp
+$ cp diff.png /tmp
 $ firefox /tmp/image.png
 $ firefox /tmp/diff.png
 
